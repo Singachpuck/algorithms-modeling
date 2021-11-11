c7fdbcbf5c4535538feda6faedf2cc0b50e276a2
Singachpuck <singachpucktv@gmail.com>
2021-11-11 16:59:09 +0200

    Lab 4

--- ЛР4/src/AbcBeeAlgorithmTest.java
+++ ЛР4/src/AbcBeeAlgorithmTest.java
@@ -24,19 +24,37 @@ class AbcBeeAlgorithmTest {
                 {1, 1, 1, 0, 1, 0, 0, 0, 1, 0}
         };
 
-        final int iterations = 100;
+//        final int[][] adjucencyMatrix = {
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+//                {0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0,},
+//                {0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0,},
+//                {0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0,},
+//                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0,},
+//                {0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0,},
+//                {0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0,},
+//                {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0,},
+//                {0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1,},
+//                {0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1,},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,},
+//        };
+
+        final int iterations = 10000;
 
         AbcBeeAlgorithm abcBeeAlgorithm = new AbcBeeAlgorithm(workerBeesCount, scoutBeesCount, adjucencyMatrix);
 
         int chromaticNumber = Integer.MAX_VALUE;
 
+        long before = System.nanoTime();
         for (int i = 0; i < iterations; i++) {
-            int cn = Arrays.stream(abcBeeAlgorithm.paint()).max().getAsInt();
+            int[] res = abcBeeAlgorithm.paint();
+            int cn = Arrays.stream(res).max().getAsInt();
 
             if (cn < chromaticNumber) {
                 chromaticNumber = cn;
+//                System.out.println(Arrays.toString(res));
             }
         }
+        System.out.println(System.nanoTime() - before);
 
         assertEquals(chromaticNumber, 3);
     }
